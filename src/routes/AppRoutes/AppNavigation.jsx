e0873c9bf2c8301c48c75fadbf9a93f3ad1dde6f
Aryan34R <aryansharma34r@gmail.com>
2025-03-20 18:06:05 +0530

    Minor Changes
    Did minor changes in navigation and helped Akashdeep with swiper and profile

--- src/routes/AppRoutes/AppNavigation.jsx
+++ src/routes/AppRoutes/AppNavigation.jsx
@@ -1,21 +1,62 @@
-import { StyleSheet} from 'react-native'
-import React from 'react'
-import HomePage from '../../pages/HomePage/HomePage'
-import Profile from '../../pages/Profile/Profile'
-import { createDrawerNavigator } from '@react-navigation/drawer'
+// import { StyleSheet} from 'react-native'
+// import React from 'react'
+// import HomePage from '../../pages/HomePage/HomePage'
+// import Profile from '../../pages/Profile/Profile'
+// import { createDrawerNavigator } from '@react-navigation/drawer'
+// import Setting from '../../pages/Setting/Setting'
+// import Searching from '../../pages/Searching/Searching'
+// import Swiper from '../../components/Swiper'
+
+// const Drawer = createDrawerNavigator();
+
+// const AppNavigation = () => {
+//   return (
+    
+//     <Drawer.Navigator initialRouteName="HomePage">
+//       <Drawer.Screen name="HomePage" component={HomePage} />
+//       <Drawer.Screen name="Searching" component={Searching} />
+//       <Drawer.Screen name="Profile" component={Profile} />
+//       <Drawer.Screen name="Setting" component={Setting} />
+//       <Drawer.Screen name="Swiper" component={Swiper} />
+//     </Drawer.Navigator>
+//   )
+// }
+
+// export default AppNavigation
+
+// const styles = StyleSheet.create({})
+
+import { StyleSheet } from 'react-native';
+import React from 'react';
+import HomePage from '../../pages/HomePage/HomePage';
+import Profile from '../../pages/Profile/Profile';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Setting from '../../pages/Setting/Setting';
+import Searching from '../../pages/Searching/Searching';
+import Swiper from '../../components/Swiper';
 
 const Drawer = createDrawerNavigator();
 
 const AppNavigation = () => {
   return (
-    
-    <Drawer.Navigator initialRouteName="HomePage">
-      <Drawer.Screen name="HomePage" component={HomePage} />
+    <Drawer.Navigator
+      initialRouteName="HomePage"
+      screenOptions={{
+        drawerActiveTintColor: '#800080', 
+        drawerActiveBackgroundColor: '#E6CCE6', 
+        headerTitle: '',
+        headerShown: true,
+      }}
+    >
+      <Drawer.Screen name="HomePage" component={HomePage}/>
+      <Drawer.Screen name="Searching" component={Searching} />
       <Drawer.Screen name="Profile" component={Profile} />
+      <Drawer.Screen name="Setting" component={Setting} />
+      <Drawer.Screen name="Swiper" component={Swiper} />
     </Drawer.Navigator>
-  )
-}
+  );
+};
 
-export default AppNavigation
+export default AppNavigation;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/routes/AuthRoutes/AuthNavigation.jsx
+++ src/routes/AuthRoutes/AuthNavigation.jsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet } from 'react-native';
+import React from 'react';
 import Login from '../../pages/Auth/Login';
 import Signup from '../../pages/Auth/Signup';
 import ForgotPassword from '../../pages/Auth/ForgotPassword';
@@ -7,24 +7,29 @@ import ResetPassword from '../../pages/Auth/ResetPassword';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AppNavigation from '../AppRoutes/AppNavigation';
+import Toast from 'react-native-toast-message';
 
 const Stack = createStackNavigator();
 
 const AuthNavigation = () => {
   return (
-    <NavigationContainer>
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Signup" component={Signup} />
-      <Stack.Screen name="ForgotPassword" component={ForgotPassword}/>
-      <Stack.Screen name="ResetPassword" component={ResetPassword}/>
-      <Stack.Screen name="AppNavigation" component={AppNavigation}  options={{ headerShown: false }} />
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="AppNavigation">
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+          <Stack.Screen name="ResetPassword" component={ResetPassword} />
+          <Stack.Screen name="AppNavigation" component={AppNavigation} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
 
-    </Stack.Navigator>
-  </NavigationContainer>
-  )
-}
+      {/* Toast Component */}
+      <Toast />
+    </>
+  );
+};
 
-export default AuthNavigation
+export default AuthNavigation;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
